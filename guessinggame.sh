1258e009c291448251790f4bbefa0148adb08474
Navsinghdeol <navsinghdeol00007@gmail.com>
2024-06-30 23:03:42 -0700

    Worked on guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,32 @@
-Guessing Game Script for the project
+#!/usr/bin/env bash
+#!File Name: guessinggame.sh
+
+function guess {
+echo "Guess how many files are in this directory?"
+echo "Please enter a number:"
+read number
+guessednumber=$number
+}
+
+guess
+
+echo "you have entered $guessednumber"
+
+nof=0
+for i in $(ls)
+do
+	let nof=$nof+1
+done
+
+while [[ $guessednumber -ne $nof ]]
+do
+	if [[ $guessednumber -gt $nof ]]
+	then
+		echo "$guessednumber is greater than the number of files in this repository. Try again"
+		guess
+	else
+		echo "$guessednumber is lesser than the number of files in this repository. Try again"
+		guess
+	fi
+done
+echo "Congratulations!! You have guessed the right number. There are $guessednumber files."	
